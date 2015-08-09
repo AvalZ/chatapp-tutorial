6639e732cf55199c8f54abec9b0f7a6c1cba583c
Andrea Valenza <avalenza89@gmail.com>
2015-08-09 20:25:39 +0200

    Add 'message inserted' event handler.

--- client/client.js
+++ client/client.js
@@ -6,3 +6,23 @@ Template.messages.helpers({
 		return Messages.find({}, {sort: {time: -1}});
 	}
 });
+
+Template.input.events = {
+	'keydown input#message': function(event) {
+		if (event.which == 13) {
+			var name = 'Anonymous';
+			var message = document.getElementById('message');
+
+			if (message.value != '') {
+				Messages.insert({
+					name: name,
+					message: message.value,
+					time: Date.now(),
+				});
+
+				document.getElementById('message').value = '';
+				message.value = '';
+			}
+		}
+	}
+}
